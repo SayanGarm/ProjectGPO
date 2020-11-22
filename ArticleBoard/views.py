463d5287906bf3e5c4cdc3e797d66604458e0f1a
SayanGarm <67288310+SayanGarm@users.noreply.github.com>
2020-11-22 20:56:22 +0700

    Начальный шаблон
    Добавил начальный шаблон с приветствием и отображением логина юзера

--- ArticleBoard/views.py
+++ ArticleBoard/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+
+def index(request) :
+    return render(request, 'ArticleBoard/index.html', {})

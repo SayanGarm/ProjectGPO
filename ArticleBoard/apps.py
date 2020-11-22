3f6ff876f888e135e210a4ed8d093478af393088
SayanGarm <67288310+SayanGarm@users.noreply.github.com>
2020-11-22 20:14:59 +0700

    Приложение "Доска статей"
    Создано приложение "Доска статей"

---
+++ ArticleBoard/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ArticleboardConfig(AppConfig):
+    name = 'ArticleBoard'
--- ProjectGPO/settings.py
+++ ProjectGPO/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ArticleBoard.apps.ArticleboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

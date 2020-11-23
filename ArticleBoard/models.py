ae1097d1d1dc8ccaaf717ae4a3d54fa64854c889
SayanGarm <67288310+SayanGarm@users.noreply.github.com>
2020-11-23 22:35:50 +0700

    Модель "Статья"
    Добавлена модель "Статья".
    Работа с ней ведется через админку

--- ArticleBoard/models.py
+++ ArticleBoard/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название')
+    content = models.TextField(null=True, blank=True, verbose_name='Описание')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Дата публикации')
+    author = models.ForeignKey(User, null=True, on_delete=models.PROTECT, verbose_name='Автор')
+    document = models.FileField()
+    
+    def delete(self, *args, **kwargs):
+        self.document.delete(save=False)
+        super().delete(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Статьи'
+        verbose_name = 'Статья'
+        ordering = ['-published']
--- ProjectGPO/urls.py
+++ ProjectGPO/urls.py
@@ -8,3 +8,7 @@ urlpatterns = [
     path('article/', include('ArticleBoard.urls'), name='main'),
     path('admin/', admin.site.urls)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

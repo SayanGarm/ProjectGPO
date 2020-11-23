bd9e382b869b905c0b0354d8e042c3aa4eb67b01
SayanGarm <67288310+SayanGarm@users.noreply.github.com>
2020-11-23 22:41:05 +0700

    Revert "Модель "Статья" исправления"
    This reverts commit 8a9ddb557b3350f43a25f53fbf717f544dcecec3.

--- ProjectGPO/urls.py
+++ ProjectGPO/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('article/', include('ArticleBoard.urls'), name='main'),
+    path('admin/', admin.site.urls)
 ]
 
 if settings.DEBUG:

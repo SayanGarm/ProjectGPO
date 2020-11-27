d5fc5651644a0b59a9a8fa3d0539784a48c21ebf
SayanGarm <67288310+SayanGarm@users.noreply.github.com>
2020-11-28 01:52:47 +0700

    Update models.py

--- Account/models.py
+++ Account/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.SET_NULL, verbose_name='Пользователь')
+    first_name = models.CharField(max_length=20,null=True, verbose_name='Имя')
+    middle_name = models.CharField(max_length=20,null=True, verbose_name='Отчество')
+    last_name = models.CharField(max_length=20,null=True, verbose_name='Фамилия')
+    email = models.EmailField(verbose_name='Email-адрес')
+    date_created = models.DateTimeField(auto_now_add=True)
+    bio = models.TextField(max_length=500, blank=True, verbose_name='Описание')
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name_plural = 'Сотрудники'
+        verbose_name = 'Сотрудник'

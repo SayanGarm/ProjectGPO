37ed2e1335f13f1b9ff16e716484d3cb369e0d42
SayanGarm <67288310+SayanGarm@users.noreply.github.com>
2020-12-01 02:20:26 +0700

    исправлено добавление статьи

--- ArticleBoard/forms.py
+++ ArticleBoard/forms.py
@@ -8,4 +8,4 @@ class ArticleListForm(forms.Form):
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = '__all__'
+        fields = ('title', 'content', 'document')
--- ArticleBoard/views.py
+++ ArticleBoard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.views import View
@@ -12,6 +12,19 @@ class ArticleCreate(View):
         context = { 'form': ArticleForm}
         return render(request, 'Article/create_article.html', context)
     def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            new_article = form.save(commit=False)
+            new_article.author = request.user
+            new_article.title = form.cleaned_data['title']
+            new_article.content = form.cleaned_data['content']
+            new_article.document = form.cleaned_data['document']
+            new_article.save()
+
+            return redirect('start-page-switch')
+           
+
 
         return render(request, 'Article/create_article.html', {})
 

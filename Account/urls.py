12fbe6290c95aacd0fa008c6b0071f827c3e29fd
SayanGarm <67288310+SayanGarm@users.noreply.github.com>
2020-12-10 22:43:22 +0700

    Добавлено восстановление пароля

--- Account/urls.py
+++ Account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetConfirmView, PasswordResetCompleteView, PasswordResetView, PasswordResetDoneView
+
 from .views import UserPage, ModeratorPage, StartPageSwitch, UsersListPage, UserProfilePage
 
 
@@ -9,4 +10,25 @@ urlpatterns = [
     path('profile/', StartPageSwitch ,name='start-page-switch'),
     path('customers/', UsersListPage.as_view() ,name='customers'),
     path('customer/<int:pk>/', UserProfilePage.as_view(), name="customer"),
+
+    path(
+        'reset_password/', 
+        PasswordResetView.as_view(template_name="reset_pass/password_reset.html"),
+        name="reset_password"
+    ),
+    path(
+        'reset_password_sent/',
+        PasswordResetDoneView.as_view(template_name="reset_pass/password_reset_sent.html"),
+        name="password_reset_done"
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(template_name="reset_pass/password_reset_form.html"),
+        name="password_reset_confirm"
+    ),
+    path(
+        'reset_password_complete/',
+        PasswordResetCompleteView.as_view(template_name="reset_pass/password_reset_done.html"),
+        name="password_reset_complete"
+    ),
 ]
--- ProjectGPO/settings.py
+++ ProjectGPO/settings.py
@@ -129,3 +129,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 LOGOUT_REDIRECT_URL = "/accounts/login/"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'stetmann007@gmail.com'
+EMAIL_HOST_PASSWORD = '7z666Ruskie800KakaRG-//-'
